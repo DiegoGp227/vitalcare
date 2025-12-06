375bdce0671e739ad402acf334c6a609b274bdd6
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2025-12-06 13:31:29 -0500

    [ add ] - docker compose and morgan

--- back/app.ts
+++ back/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
-
+import morgan from "morgan"
 // - variables
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// - midelware
+app.use(morgan("dev"))
 
 app.get("/ping", (_req, res) => {
   res.json({
