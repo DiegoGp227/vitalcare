4e0f4b4396b9f65ee5b3a82073240d9629449197
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2025-12-06 23:02:11 -0500

    [ add ] - router

--- back/app.ts
+++ back/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import morgan from "morgan";
 // routers
 import user_router from "./routers/user/users_router.js";
+import symptoms_router from "./routers/symptoms/symptoms_routers";
 
 // - variables
 const app = express();
@@ -19,6 +20,7 @@ app.get("/ping", (_req, res) => {
 });
 // router
 app.use("/user", user_router);
+app.use("/symptoms", symptoms_router);
 // - listen
 app.listen(PORT, () => {
   console.log(`server init in port ${PORT} ...`);
---
+++ back/routers/symptoms/symptoms_routers.ts
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import axios from "axios";
+
+const router = Router();
+
+interface routerProp {
+  id: number;
+}
+
+const HASURA_GRAPHQL_ENDPOINT =
+  process.env.GRAPHQL_API || "https://main-hermit-36.hasura.app/v1/graphql";
+const HASURA_ADMIN_SECRET =
+  process.env.ADMIN_HASURA ||
+  "IAQXSF0JRCFC2ylKMuD6ZNnzdhKc69iSbxwTuG9EWEdy1CsLYsrmPWzBpqhh14Bc";
+
+router.post("/", async (req, res) => {
+  const { id }: routerProp = req.body;
+  console.log(id);
+  const query = `
+  query MyQuery($id : int!) {
+    paciente(where: {id : {_eq : $id}}){
+      apellido
+      cedula
+      id
+      fechanacimiento
+      contrase_a
+      demo
+      direccion
+      email
+      genero
+      sintomas {
+        demo
+        dolor
+        fecha
+        id
+      }
+    }
+  }
+  `;
+  const response = await axios.post(
+    HASURA_GRAPHQL_ENDPOINT,
+    { query: query },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
+      },
+    }
+  );
+  res.json({
+    data: response.data.data,
+  });
+});
+
+export default router;
