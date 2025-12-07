77743a449fb8405a5136005cede856678b02b8b3
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2025-12-06 23:31:59 -0500

    [ add ] - router create

--- back/routers/user/users_router.ts
+++ back/routers/user/users_router.ts
@@ -20,6 +20,12 @@ interface ILoginUser {
   cc: number;
 }
 
+const HASURA_GRAPHQL_ENDPOINT =
+  process.env.GRAPHQL_API || "https://main-hermit-36.hasura.app/v1/graphql";
+const HASURA_ADMIN_SECRET =
+  process.env.ADMIN_HASURA ||
+  "IAQXSF0JRCFC2ylKMuD6ZNnzdhKc69iSbxwTuG9EWEdy1CsLYsrmPWzBpqhh14Bc";
+
 router.post("/register", (req, res) => {
   const body: IRegisterUser = req.body;
   console.log(body);
@@ -28,10 +34,45 @@ router.post("/register", (req, res) => {
   });
 });
 
-router.post("/login", (req, res) => {
-  const body: ILoginUser = req.body;
-  console.log(body);
-  res.json({ message: "login success" });
+router.post("/login", async (req, res) => {
+  const query = `
+  query MyQuery($cc : Int!) {
+    paciente(where: { cedula: { _eq: $cc } }){
+      apellido
+      cedula
+      id
+      fechanacimiento
+
+      direccion
+      email
+      genero
+      sintomas {
+        demo
+        dolor
+        fecha
+        id
+      }
+    }
+  }
+  `;
+  const { cc }: ILoginUser = req.body;
+  console.log(cc);
+  const response = await axios.post(
+    HASURA_GRAPHQL_ENDPOINT,
+    {
+      query: query,
+      variables: { cc: cc },
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": HASURA_ADMIN_SECRET,
+      },
+    }
+  );
+  res.json({
+    data: response.data.data,
+  });
 });
 
 export default router;
